50ec2601a290c381ea2290da3d841edb7699e8e4
Agustin Molina <am.motos@hotmail.com>
2023-10-16 00:55:48 -0300

    1.0

--- src/libs/prisma.ts
+++ src/libs/prisma.ts
@@ -1,9 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-  var prisma: PrismaClient;
+  var prisma: PrismaClient | undefined;
 }
 
 export const prisma = global.prisma || new PrismaClient();
 
-if (process.env.NODE === "development") global.prisma = prisma;
+if (process.env.NODE !== "production") global.prisma = prisma;
